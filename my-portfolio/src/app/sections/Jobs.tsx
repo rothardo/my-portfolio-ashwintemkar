1affb0920990fe9e1d545c897054f1d91eabbdc5
Ashwin Temkar <ashwintemkar@gmail.com>
2024-09-11 07:26:04 +0530

    changed the job section

--- my-portfolio/src/app/sections/Jobs.tsx
+++ my-portfolio/src/app/sections/Jobs.tsx
@@ -1,9 +1,29 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import { jobData } from "../data/data";
+import { Job } from "../types/type";
 
 const Jobs: FC = () => {
+  const [selectedJob, setSelectedJob] = useState<Job | null>(jobData[0]);
+
   return (
-    <div>
-      <h1>Jobs</h1>
+    <div style={{ display: "flex" }}>
+      <nav style={{ width: "200px", borderRight: "1px solid #ccc" }}>
+        <h2>Job Experiences</h2>
+        <ul>
+          {jobData.map((job) => (
+            <li
+              key={job.id}
+              onClick={() => setSelectedJob(job)}
+              style={{ cursor: "pointer", padding: "5px" }}
+            >
+              {job.title}
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <div style={{ padding: "20px" }}>
+        {selectedJob ? <></> : <p>Select a job to see details.</p>}
+      </div>
     </div>
   );
 };
