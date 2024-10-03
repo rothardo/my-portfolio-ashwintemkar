7a9e36d08201ecd2f85199545338d89098ac7130
Ashwin Temkar <ashwintemkar@gmail.com>
2024-10-03 17:29:56 +0000

    tailwind config

--- my-portfolio/tailwind.config.ts
+++ my-portfolio/tailwind.config.ts
@@ -9,6 +9,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      animation: {
+        "fade-in": "fadeIn 1s ease-out forwards",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0", transform: "translateY(50px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+      },
       spacing: {
         "1/10": "10%",
       },
