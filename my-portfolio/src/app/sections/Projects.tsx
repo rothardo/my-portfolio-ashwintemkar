482392d72250c151f2fb9eb745d3b412e6435991
Ashwin Temkar <ashwintemkar@gmail.com>
2024-10-04 18:40:56 +0000

    upgrade the projects component

--- my-portfolio/src/app/sections/Projects.tsx
+++ my-portfolio/src/app/sections/Projects.tsx
@@ -1,10 +1,22 @@
 import { FC } from "react";
+import SectionContainer from "@/components/SectionContainer";
+import Card from "@/components/Card";
+import { projectData } from "../data/data";
 
 const Projects: FC = () => {
   return (
-    <div>
-      <h1>Projects</h1>
-    </div>
+    <SectionContainer>
+      <div className="max-w-4xl mx-auto mb-6">
+        <h2 className="text-left text-3xl font-extrabold text-accent-light dark:text-white">
+          Projects
+        </h2>
+      </div>
+      <div className="flex flex-col max-w-4xl mx-auto space-y-8">
+        {projectData.map((project) => (
+          <Card key={project.id} card={project} />
+        ))}
+      </div>
+    </SectionContainer>
   );
 };
 
